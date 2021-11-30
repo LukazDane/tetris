3ddf95f701a191f7bfcd8bcb03ef953609773652
Luke <deandrevidal@aol.com>
2021-11-30 15:56:32 -0800

    Resume and pause implemented

--- src/components/GridBoard/GridBoard.js
+++ src/components/GridBoard/GridBoard.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import GridSquare from "../GridSquare/GridSquare";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { shapes } from "../../utils";
+import { moveDown } from "../../actions";
+
 // Represents a 10 x 18 grid of grid squares
 
 export default function GridBoard(props) {
+  const requestRef = useRef();
+  const lastUpdateTimeRef = useRef(0);
+  const progressTimeRef = useRef(0);
+  const dispatch = useDispatch();
+
   const game = useSelector((state) => state.game);
   const { grid, shape, rotation, x, y, isRunning, speed } = game;
   // generates an array of 18 rows, each containing 10 GridSquares.
   const block = shapes[shape][rotation];
   const blockColor = shape;
+  const update = (time) => {
+    requestRef.current = requestAnimationFrame(update);
+    if (!isRunning) {
+      return;
+    }
+    if (!lastUpdateTimeRef.current) {
+      lastUpdateTimeRef.current = time;
+    }
+    const deltaTime = time - lastUpdateTimeRef.current;
+    progressTimeRef.current += deltaTime;
+    if (progressTimeRef.current > speed) {
+      dispatch(moveDown());
+      progressTimeRef.current = 0;
+    }
+    lastUpdateTimeRef.current = time;
+  };
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(update);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, [isRunning]);
   // map rows
   const gridSquares = grid.map((rowArray, row) => {
     // map columns
--- src/components/ScoreBoard/ScoreBoard.js
+++ src/components/ScoreBoard/ScoreBoard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { pause, resume, restart } from "../actions";
+import { pause, resume, restart } from "../../actions";
 
 export default function ScoreBoard(props) {
   const dispatch = useDispatch();
